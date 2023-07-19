0d83576ab8fe0245057a111434994c05c2b3a095
Rahul Rajan <rahulrajancc@gamil.com>
2023-07-19 06:55:03 +0530

    made changes in post and connected to firebasr

--- src/componets/feed/feedpost.jsx
+++ src/componets/feed/feedpost.jsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import './feedpost.css';
+
 import Posts from './posts/posts';
-class Feedpost extends Component {
-    render() {
-        return (
-            <div className='feedpost_cls'> 
-                <div className="post">
-                    <input type="text" name="Post" id="Post_things" />
-                    <input type="file" name="Add file to share" id="Add_file" />
-                  
-                </div ><div className="post_pla">< Posts />< Posts />< Posts /></div>
-                < Posts />< Posts />< Posts />
-            </div>
-        );
-    }
+import { useState,useEffect } from 'react';
+import { storage } from '../../firebase';
+import { ref ,uploadBytes, listAll, getDownloadURL} from 'firebase/storage';
+import {v4} from 'uuid'
+
+
+
+const Feedpost = () => {
+    const [Imgupload,setImgupload]=useState(null)
+    const imgupload=() =>{
+    if (Imgupload==null){return;}
+    const imgageref=ref(storage,`image/${Imgupload.name +v4() }`)
+    uploadBytes(imgageref,Imgupload).then(() =>{
+        alert('have')
+    })
+
+}
+
+
+   return (
+        <div className='feedpost_cls'> 
+        <div className="post">
+            <input type="text" name="Post" id="Post_things" />
+            <input type="file" name="Add file to share" id="Add_file" onChange={(event)=>{setImgupload(event.target.files[0])}} />
+            <input type="submit" name="submitPost" id="Post_submit" onClick={imgupload} />
+          
+        
+        </div ><div className="post_pla">< Posts /></div>
+        
+    </div>
+    );
 }
 
 export default Feedpost;
+
--- src/componets/feed/posts/posts.jsx
+++ src/componets/feed/posts/posts.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import './posts.css';
+import { useState,useEffect } from 'react';
+import { storage } from '../../../firebase';
+import { ref ,uploadBytes, listAll, getDownloadURL} from 'firebase/storage';
 
-class Posts extends Component {
-    render() {
-        return (
-            <div className='Post-section'><section className="post_comp"><input type="image" src={require('../../../assest/testaccount.png')} alt="" />
+
+
+const Posts = () => {
+    const [Imglist, setImglist] = useState([]);
+    const imgreflist=ref(storage,"image/")
+
+
+    useEffect(() =>{
+        listAll(imgreflist).then((response) =>{
+            response.items.forEach((item) => {
+                getDownloadURL(item).then((url) =>{
+                    setImglist((prev) => [...prev,url]);
+                })
+                
+            });
+        })
+    },[]);
+    return (
+        <div className='Post-section'><section className="post_comp"><input type="image" src={require('../../../assest/testaccount.png')} alt="" />
                 <span id='Username'>Rahul Rajan</span><br />
                 <span className='Added_Date' style={{color:'purple'}}>03/07/2023</span></section>
                 
-                <section className="post_img">
-                    <input type="image" src={require('../../../assest/post.jpg')} height='40%' width='500vw' alt="" />
-                </section>
-                <section className="like_share_com">
+                    {Imglist.map((url) =>{
+                   return <><section className="post_img">
+                    
+                     <img src={url} height='40%' width='450vw' alt="" />
+                    </section>
+                    <section className="like_share_com">
                     <img src={require('../../../assest/icons8-like-50.png')} alt="like_pic" className="like" />
                     <img src={require('../../../assest/icons8-comment-50.png')} alt="like_pic" className="like" />
                     <img src={require('../../../assest/icons8-share-50.png')} alt="like_pic" className="like" />
                 </section>
+                    </>
+                })}
+               
             </div>
-        );
-    }
+    );
 }
 
 export default Posts;
+
